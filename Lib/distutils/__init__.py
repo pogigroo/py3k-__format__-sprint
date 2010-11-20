79f59f8d1209003d11fa3d004c7be8f6b0e4c790
Vlad Korolev <vlad@v-lad.org>
2010-11-20 10:56:04 -0500

    Initial Commit

---
+++ Lib/distutils/__init__.py
@@ -0,0 +1,19 @@
+"""distutils
+
+The main package for the Python Module Distribution Utilities.  Normally
+used from a setup script as
+
+   from distutils.core import setup
+
+   setup (...)
+"""
+
+__revision__ = "$Id: __init__.py 86446 2010-11-13 06:39:58Z georg.brandl $"
+
+# Distutils version
+#
+# Updated automatically by the Python release process.
+#
+#--start constants--
+__version__ = "3.2a4"
+#--end constants--
